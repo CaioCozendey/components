cfc13b7394045318442dde010ca57caca8befa3e
Caio Dias Cozendey <caiocozendey@hotmail.com>
2023-06-28 20:00:04 -0300

    Props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,40 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='items-center flex flex-col'>
+    <div className='items-center flex flex-col m-2'>
       <h1 className='text-4xl mt-4'> Components </h1>
       <h2 className='text-2xl mt-4'> Cards </h2>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 lg:gap-4 md:gap-1">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="mt-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 lg:gap-12 md:gap-4 gap-8">
+        <Card 
+          nome='Caio'
+          descricao='Pão • Carne • Queijo • Salada • Milho'
+          preco='R$18,99'
+          imagem='/hamburguer2.jpg'
+        />
+        <Card 
+          nome='Caio2'
+          descricao='Pão • Carne • Queijo • Salada • Milho'
+          preco='R$18,99'
+          imagem='/hamburguer.jpg'
+        />
+        <Card 
+          nome='Caio3'
+          descricao='Pão • Carne • Queijo • Salada • Milho'
+          preco='R$18,99'
+          imagem='/hamburguer2.jpg'
+        />
+        <Card 
+          nome='Caio4'
+          descricao='Pão • Carne • Queijo • Salada • Milho'
+          preco='R$18,99'
+          imagem='/hamburguer.jpg'
+        />
+        <Card 
+          nome='Caio5'
+          descricao='Pão • Carne • Queijo • Salada • Milho'
+          preco='R$18,99'
+          imagem='/hamburguer2.jpg'
+        />
       </div>
     </div>
   )
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,23 +1,30 @@
 import Image from "next/image";
 
-export default function Card() {
+interface Card{
+    nome: string;
+    descricao: string;
+    preco: string;
+    imagem: string;
+}
+
+export default function Card(props: Card) {
     return (
         <div>
             <button>
                 <div>
-                    <div className="p-6">
+                    <div>
                         <div>
                             <div className="overflow-hidden rounded-2xl bg-gray-50 max-w-lg">
                                 <div className="flex flex-col items-center overflow-hidden">
-                                    <Image src="/hamburguer2.jpg" width={512} height={512} alt="Hamburger" />
+                                    <Image src={props.imagem} width={512} height={480} alt="Hamburger" />
                                 </div>
                                 <div className="p-6">
                                     <div className="md:flex-row items-center justify-between sm:flex-row sm:items-center">
                                         <div className="break-normal">
-                                            <h2 className="mt-2 text-lg font-semibold text-gray-800"> X-Bacon </h2>
-                                            <p className="text-gray-400"> Pão • Carne • Queijo • Salada • Milho</p>
+                                            <h2 className="mt-2 text-lg font-semibold text-gray-800"> {props.nome} </h2>
+                                            <p className="text-gray-400">{props.descricao}</p>
                                         </div>
-                                        <span className="mt-4 inline-block rounded-full bg-orange-400 p-3 text-xl font-medium text-white hover:bg-orange-500 duration-200 min-w-full"> R$18,99 </span>
+                                        <span className="mt-4 inline-block rounded-full bg-orange-400 p-3 text-xl font-medium text-white hover:bg-orange-500 duration-200 min-w-full"> {props.preco} </span>
                                     </div>
                                 </div>
                             </div>
